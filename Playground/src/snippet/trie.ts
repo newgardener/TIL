d686e66d698d460067a4bf42e8047917a1d9790e
newgardener <jennyshin106@gmail.com>
2024-10-29 00:44:19 +0900

    feat: implement Trie structure

---
+++ Playground/src/snippet/trie.ts
@@ -0,0 +1,50 @@
+class TrieNode {
+  children: Map<string, TrieNode>;
+  isEndOfWord: boolean;
+
+  constructor() {
+    this.children = new Map<string, TrieNode>();
+    this.isEndOfWord = false;
+  }
+}
+
+class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word: string) {
+    let current = this.root;
+    for (const char of word) {
+      if (!current.children.has(char)) {
+        current.children.set(char, new TrieNode());
+      }
+      current = current.children.get(char)!;
+    }
+    current.isEndOfWord = true;
+  }
+
+  search(word: string) {
+    let current = this.root;
+    for (const char of word) {
+      if (!current.children.has(char)) {
+        return false;
+      }
+      current = current.children.get(char)!;
+    }
+    return current.isEndOfWord;
+  }
+
+  startsWith(prefix: string) {
+    let current = this.root;
+    for (const char of prefix) {
+      if (!current.children.has(char)) {
+        return false;
+      }
+      current = current.children.get(char)!;
+    }
+    return true;
+  }
+}
